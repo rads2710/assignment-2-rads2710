4cbd176dcdfb23e1c3b4a504f81cf3dfb84c7910
rads2710 <61345984+rads2710@users.noreply.github.com>
2020-03-28 21:03:50 +0530

    Circular Queue created.

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -5,6 +5,82 @@
  *  Time: 7:06 PM
  */
 package problem5.circularqueue;
+
+import problem5.node.Node;
+import problem5.student.Student;
+
 //to implement circular queue
-public class MyCircularQueue {
+public class MyCircularQueue
+{
+    Node front;
+    Node rear;
+    int size;
+
+    public MyCircularQueue()
+    {
+        front=null;
+        rear=null;
+        size=0;
+    }
+
+    boolean isEmpty()
+    {
+        boolean response=false;
+        if(front == null)
+        {
+            response=true;
+        }
+        return response;
+    }
+
+    public void enqueue(Student element)
+    {
+        Node node=new Node(element);
+        size++;
+
+        if(isEmpty())
+        {
+            front=node;
+            rear=node;
+            rear.setNext(front);
+        }
+        else
+        {
+            rear.setNext(node);
+            rear=node;
+            rear.setNext(front);
+        }
+    }
+    public void process()
+    {
+        int counter=size;
+        while(counter>0)
+        {
+            counter--;
+            Student s=front.getData();
+            if(s.getBackLog()-s.getAppearingCount() <= 0 )
+            {
+                rear.setNext(front.getNext());
+                front=front.getNext();
+            }
+            else
+            {
+                front = front.getNext();
+                rear = rear.getNext();
+            }
+        }
+
+    }
+
+    public void display()
+    {
+        Node temp = front;
+        System.out.println();
+        while(temp.getNext()!=front)
+        {
+            System.out.println(temp.getData());
+            temp = temp.getNext();
+        }
+        System.out.println(temp.getData());
+    }
 }
