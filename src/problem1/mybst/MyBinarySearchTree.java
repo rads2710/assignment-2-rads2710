de60962ee242fc6175f63714bda987eaaab8f451
rads2710 <61345984+rads2710@users.noreply.github.com>
2020-03-28 12:38:27 +0530

    created BST

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,80 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
-public class MyBinarySearchTree {
+public class MyBinarySearchTree
+{
+    TreeNode root;
+
+    public TreeNode getRoot()
+    {
+        return root;
+    }
+
+    public void insert(int data)
+    {
+        TreeNode node= new TreeNode(data);
+        if(root == null)
+        {
+            root=node;
+        }
+        else
+        {
+            TreeNode temp=root;
+            TreeNode parent=null;
+            while( temp != null)
+            {
+                parent=temp;
+                if( data<= temp.getData())
+                {
+                    temp=temp.getLeft();
+                }
+                else
+                {
+                    temp=temp.getRight();
+                }
+            }
+            if(node.getData()<= parent.getData())
+            {
+                parent.setLeft(node);
+            }
+            else
+            {
+                parent.setRight(node);
+            }
+        }
+    }
+
+    public void preOrderTraverse()
+    {
+        preOrder(root);
+    }
+
+    void preOrder(TreeNode node)
+    {
+        if(node != null)
+        {
+            System.out.print(node.getData()+" ");
+            preOrder(node.getLeft());
+            preOrder(node.getRight());
+        }
+    }
+
+    public void postOrderTraverse()
+    {
+        postOrder(root);
+    }
+
+    void postOrder(TreeNode node)
+    {
+        if(node != null)
+        {
+            postOrder(node.getLeft());
+            postOrder(node.getRight());
+            System.out.print(node.getData()+" ");
+        }
+    }
 }
