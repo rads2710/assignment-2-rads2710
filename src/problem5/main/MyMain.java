2d4b7923200ff93f14a8e6a96d425d54e69435c6
rads2710 <61345984+rads2710@users.noreply.github.com>
2020-03-28 21:24:10 +0530

    Problem 5 solved.

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -5,6 +5,23 @@
  *  Time: 7:06 PM
  */
 package problem5.main;
+
+import problem5.circularqueue.MyCircularQueue;
+import problem5.student.Student;
+
 //executable class
-public class MyMain {
+public class MyMain
+{
+    public static void main(String[] args) {
+        MyCircularQueue obj = new MyCircularQueue();
+        obj.enqueue(new Student(1,"A",4,0));
+        obj.enqueue(new Student(2,"B",4,1));
+        obj.enqueue(new Student(3,"C",4,4));
+        obj.enqueue(new Student(4,"D",2,1));
+        obj.enqueue(new Student(5,"E",2,2));
+        obj.enqueue(new Student(6,"F",0,0));
+        obj.process();
+        obj.display();
+
+    }
 }
