acd766ba1b02aed7318255524f6d7450736b44a8
rads2710 <61345984+rads2710@users.noreply.github.com>
2020-03-28 20:20:33 +0530

    problem 1 solved.

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,21 @@
 package problem1.main;
 // executable class
 
-public class MyMain {
+import problem1.mybst.MyBinarySearchTree;
+
+public class MyMain
+{
+    public static void main(String[] args)
+    {
+        MyBinarySearchTree tree=new MyBinarySearchTree();
+        tree.insert(5);
+        tree.insert(1);
+        tree.insert(10);
+        tree.insert(4);
+        tree.insert(6);
+        System.out.println("ALL LEFT NODES ARE:");
+        tree.printLeftNode();
+        System.out.println("COUNT OF NODES NOT HAVING NODE");
+        tree.countNotLeft();
+    }
 }
