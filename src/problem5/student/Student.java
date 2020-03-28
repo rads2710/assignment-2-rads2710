d2a70b5adb2c4537d7c4f077fc5bfe4fa39e0c63
rads2710 <61345984+rads2710@users.noreply.github.com>
2020-03-28 21:05:37 +0530

    Student class created.

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -6,5 +6,63 @@
  */
 package problem5.student;
 // to store student information and properties
-public class Student {
+public class Student
+{
+    private int rollNo;
+    private String name;
+    private int backLog;
+    private int appearingCount;
+
+    public Student(int rollNo, String name)
+    {
+        this.rollNo = rollNo;
+        this.name = name;
+    }
+
+    public Student(int rollNo, String name, int backLog, int appearingCount) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.backLog = backLog;
+        this.appearingCount = appearingCount;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBackLog() {
+        return backLog;
+    }
+
+    public void setBackLog(int backLog) {
+        this.backLog = backLog;
+    }
+
+    public int getAppearingCount() {
+        return appearingCount;
+    }
+
+    public void setAppearingCount(int appearingCount) {
+        this.appearingCount = appearingCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "rollNo=" + rollNo +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
